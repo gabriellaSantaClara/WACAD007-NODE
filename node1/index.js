a759cc0f0a16d1afb7908de16d3212abcc6966b4
Gabriella Mendonça Santa Clara <31377559+gabriellaSantaClara@users.noreply.github.com>
2025-06-04 00:06:28 -0400

    Add files via upload

--- node1/index.js
+++ node1/index.js
@@ -1,9 +1,49 @@
-var http = require('http');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const { createLink } = require('./util');
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });  //variavel ambiente NODE_ENV exercicio 4
+
+const folder = process.argv[2] || '.';
+const PORT = process.env.PORT || 3333;
+
+const server = http.createServer((req, res) => {
+const url = req.url;
+const filepath = folder + url;
+
+  if (url === '/') {
+    fs.readdir(folder, (err, files) => {   //readdir do fs exercicio 1
+
+      //res.write("Instituto de Computação");
+      res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
+      files.forEach(file => res.write(createLink(file)));
+      res.end();
+    });
+  } else {
+    fs.readFile(filepath, 'utf-8', (err, data) => {
+      res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' }); //ler arquivo fs
+      res.write(`<a href="/">Voltar</a><br><br>`);
+      res.write(`<pre>${data}</pre>`);
+      res.end();
+    });
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
+
+
+/*var http = require('http');
 var fs = require('fs');
 var path = require('path');
 
 // Obtém o diretório passado como argumento na linha de comando
 var directory = process.argv[2];
+
+const dotenv = require("dotenv")
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
+
 
 if (!directory) {
   console.error('Por favor, informe o nome do diretório como argumento.');
@@ -33,4 +73,4 @@ var server = http.createServer((req, res) => {
 var PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
+});*/
